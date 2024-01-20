f1c9e7710726d30aa6ae1394da3dc0158c68e3d7
Nikolaj <nikolaj.tetradov@gmail.com>
2024-01-20 15:29:46 +0200

    modify emailjs

--- app/(user)/contact/page.tsx
+++ app/(user)/contact/page.tsx
@@ -14,11 +14,12 @@ const metadata: Metadata = {
 };
 
 // const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
-// const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+// const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
 
 const serviceId = 'service_01cdr11';
-const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
-const publicKey = 'E0ZusFaL6tRW0y_Fo';
+// const publicKey = '';
+const templateId = 'template_h6hfhke';
 
 if (!serviceId || !templateId || !publicKey) {
   console.error('One or more EmailJS variables are not defined.');
